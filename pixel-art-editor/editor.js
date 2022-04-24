c725a1b89d23561b416dbd392a2d87e0f6d62464
Toby Law <toby@tzcl.me>
2022-04-24 13:56:22 +1000

    Added loading/saving

--- pixel-art-editor/editor.js
+++ pixel-art-editor/editor.js
@@ -137,7 +137,6 @@ function pointerPosition(pos, dom) {
   };
 }
 
-// Treat as immutable (why?)
 class Picture {
   constructor(width, height, pixels) {
     this.width = width;
@@ -295,6 +294,99 @@ class ColourSelect {
   }
 }
 
+class SaveButton {
+  constructor(state) {
+    this.picture = state.picture;
+    this.dom = elt(
+      "button",
+      {
+        onclick: () => this.save(),
+      },
+      "💾 Save"
+    );
+  }
+
+  save() {
+    let canvas = elt("canvas");
+    drawPicture(this.picture, canvas, 1);
+    let link = elt("a", {
+      href: canvas.toDataURL(),
+      download: "pixelart.png",
+    });
+
+    // Simulate clicking the link and then remove it
+    document.body.appendChild(link);
+    link.click();
+    link.remove();
+  }
+
+  update(state) {
+    this.picture = state.picture;
+  }
+}
+
+class LoadButton {
+  constructor(_, { dispatch }) {
+    this.dom = elt(
+      "button",
+      {
+        onclick: () => startLoad(dispatch),
+      },
+      "📂 Load"
+    );
+  }
+
+  update() {}
+}
+
+function startLoad(dispatch) {
+  let input = elt("input", {
+    type: "file",
+    onchange: () => finishLoad(input.files[0], dispatch),
+  });
+  document.body.appendChild(input);
+  input.click();
+  input.remove();
+}
+
+function finishLoad(file, dispatch) {
+  if (file == null) return;
+  let reader = new FileReader();
+  reader.addEventListener("load", () => {
+    let image = elt("img", {
+      onload: () => dispatch({ picture: pictureFromImage(image) }),
+      src: reader.result,
+    });
+  });
+  reader.readAsDataURL(file);
+}
+
+function pictureFromImage(image) {
+  // Limit size of pictures to 100x100
+  let width = Math.min(100, image.width);
+  let height = Math.min(100, image.height);
+
+  let canvas = elt("canvas", { width, height });
+  let cx = canvas.getContext("2d");
+
+  cx.drawImage(image, 0, 0);
+  let pixels = [];
+  let { data } = cx.getImageData(0, 0, width, height);
+
+  const hex = (n) => {
+    return n.toString(16).padStart(2, "0");
+  };
+
+  for (let i = 0; i < data.length; i += 4) {
+    let [r, g, b] = data.slice(i, i + 3);
+    pixels.push("#" + hex(r) + hex(g) + hex(b));
+  }
+
+  return new Picture(width, height, pixels);
+}
+
+// TODO: reorganise code, clean up
+
 // Start application
 let state = {
   tool: "draw",
@@ -304,7 +396,7 @@ let state = {
 
 let app = new PixelEditor(state, {
   tools: { draw, fill, rectangle, pick },
-  controls: [ToolSelect, ColourSelect],
+  controls: [ToolSelect, ColourSelect, SaveButton, LoadButton],
   dispatch(action) {
     state = updateState(state, action);
     app.update(state);
